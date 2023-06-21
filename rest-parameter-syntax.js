a47fa3e264ab2e65f808cdca3132338602848a98
Natalitta <nataliialebedieva@gmail.com>
2023-06-21 20:06:22 +0000

    Add rest examples

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -4,7 +4,21 @@
  */
 
 // Regular function call 
+const addAll = (a, b, c) => a + b + c;
+let sum = addAll(1, 2, 3);
+console.log("Sum:", sum);
 
 // Extra arguments are ignored
+let sum2 = addAll(1, 2, 3, 4, 5);
+console.log(sum2);
 
 // Function using ...rest
+let sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest) {
+        sum += i;
+    }
+    return sum;
+};
+let sum3 = sumRest(1, 2, 3, 4, 5);
+console.log(sum3);
